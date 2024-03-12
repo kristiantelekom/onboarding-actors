96ed70c76c3c2ccd0f9b1d2666800a2880826ead
Kristian Klečina <kristiankl@telekom.rs>
2024-03-12 08:58:30 +0100

    first commit

---
+++ src/main/java/com/onboarding/actors/OnboardingActorsApplication.java
@@ -0,0 +1,13 @@
+package com.onboarding.actors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingActorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingActorsApplication.class, args);
+	}
+
+}
